ba8a4c42e8e03b3dd59875e5849e4b7f52d39af6
Leonardo Patrick <leonardop@itout.com.br>
2021-03-05 17:03:09 -0300

    Swagger com bearer-jwt

--- backend/src/main/java/br/com/feiranamao/config/SecurityConfig.java
+++ backend/src/main/java/br/com/feiranamao/config/SecurityConfig.java
@@ -1,18 +1,141 @@
 package br.com.feiranamao.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	 @Override
+	public void configure(HttpSecurity http) throws Exception {
+ 
+		  http.authorizeRequests()
+	        .antMatchers("/login",
+	         		  "/ide/**"
+	        		,"/v2/"
+	        		,"/v2/**"
+        		  , "/resources/**"
+        		  ,"/webjars/**", "/swagger/**"
+        		  ,"/css/**"
+        		  ,"/configuration/**"
+        		  ,"/swagger-resources/**"
+        		  , "/v2/api-docs",
+	        		"/swagger/**",
+	        		"/v3/**"
+	        		,"/v2/api-docs/**"
+	        		,"/api-docs/"
+	        		,"/api-docs/**"
+	        		,"/v3/api-docs"
+	        		,"/v3/api-docs/**", 
+	        		"/configuration/ui",
+	        		"/swagger-resources",
+	        		"/configuration/security",
+	        		"/swagger-ui.html",
+	        		"/webjars/**",
+	        		"/swagger-resources/configuration/ui", 
+	        		"/swagger-ui.html",
+	        		"/swagger-resources/configuration/security",
+	        		"/swagger-ui/**"
+	        		,"/error/**"
+	        		,"/v2/api-docs/.yaml"
+	        		,"/v2/api-docs/swagger-config"
+	        		,"/participant/**"
+	        		,"/swagger-ui/"
+	        		,"/swagger-ui/**"
+	        		,"/swagger-ui"
+	        		).permitAll()
+	        .antMatchers("/admin/**").hasRole("ADMIN")
+	        .antMatchers("/**","/produtos/**","/produtos").hasAnyRole("ADMIN", "USER")
+	        .and().formLogin()
+	        .and().logout().logoutSuccessUrl("/login").permitAll()
+	        .and().csrf().disable();
+			
+	  }
+	  
+	//  @Override
+      public void configure(WebSecurity web) throws Exception {
+              web.ignoring()
+              // Spring Security should completely ignore URLs starting with /resources/
+                              .antMatchers("/login",
+                	         		  "/ide/**"
+                  	        		,"/v2/"
+                  	        		,"/v2/**"
+                            		  , "/resources/**"
+                            		  ,"/webjars/**", "/swagger/**"
+                            		  ,"/css/**"
+                            		  ,"/configuration/**"
+                            		  ,"/swagger-resources/**"
+                            		  , "/v2/api-docs",
+                  	        		"/swagger/**",
+                  	        		"/v3/**"
+                  	        		,"/v2/api-docs/**"
+                  	        		,"/api-docs/"
+                  	        		,"/api-docs/**"
+                  	        		,"/v3/api-docs"
+                  	        		,"/v3/api-docs/**", 
+                  	        		"/configuration/ui",
+                  	        		"/swagger-resources",
+                  	        		"/configuration/security",
+                  	        		"/swagger-ui.html",
+                  	        		"/webjars/**",
+                  	        		"/swagger-resources/configuration/ui", 
+                  	        		"/swagger-ui.html",
+                  	        		"/swagger-resources/configuration/security",
+                  	        		"/swagger-ui/**"
+                  	        		,"/error/**"
+                  	        		,"/v2/api-docs/.yaml"
+                  	        		,"/v2/api-docs/swagger-config"
+                  	        		,"/participant/**"
+                  	        		,"/swagger-ui/"
+                  	        		,"/swagger-ui/**"
+                  	        		,"/swagger-ui");
+      }
 
 }
+
+/*@EnableAuthorizationServer
+@EnableResourceServer*/
+
+/*"/**","/login",
+	         		  "/ide/**"
+	        		,"/v2/"
+	        		,"/v2/**"
+          		  , "/resources/**"
+          		  ,"/webjars/**", "/swagger/**"
+          		  ,"/css/**"
+          		  ,"/configuration/**"
+          		  ,"/swagger-resources/**"
+          		  , "/v2/api-docs",
+	        		"/swagger/**",
+	        		"/v3/**"
+	        		,"/v2/api-docs/**"
+	        		,"/api-docs/"
+	        		,"/api-docs/**"
+	        		,"/v3/api-docs"
+	        		,"/v3/api-docs/**", 
+	        		"/configuration/ui",
+	        		"/swagger-resources",
+	        		"/configuration/security",
+	        		"/swagger-ui.html",
+	        		"/webjars/**",
+	        		"/swagger-resources/configuration/ui", 
+	        		"/swagger-ui.html",
+	        		"/swagger-resources/configuration/security",
+	        		"/swagger-ui/**"
+	        		,"/error/**"
+	        		,"/v2/api-docs/.yaml"
+	        		,"/v2/api-docs/swagger-config"
+	        		,"/participant/**"
+	        		,"/swagger-ui/"
+	        		,"/swagger-ui/**"
+	        		,"/swagger-ui"*/
---
+++ backend/src/main/java/br/com/feiranamao/config/SwaggerConfiguration.java
@@ -0,0 +1,123 @@
+package br.com.feiranamao.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfiguration implements WebFluxConfigurer { 
+
+	@Bean
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	      .apiInfo(apiInfo())
+	      .securityContexts(Arrays.asList(securityContext()))
+	      .securitySchemes(Arrays.asList(apiKey()))
+	      .select()
+	      .apis(RequestHandlerSelectors.basePackage("br.com.feiranamao"))  
+	      .paths(PathSelectors.any())
+	      .build();
+	} 
+	
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				/* .components(new Components()
+	                        .addSecuritySchemes("spring_oauth", new SecurityScheme()
+	                                .type(SecurityScheme.Type.OAUTH2)
+	                                .description("Oauth2 flow")
+	                            .flows(new OAuthFlows()
+	                                        .authorizationCode(new OAuthFlow()
+	                                                .authorizationUrl("localhost" + "/auth")
+	                                                .refreshUrl("localhost" + "/token")
+	                                                .tokenUrl("localhost" + "/token")
+	                                                .scopes(new Scopes())
+	                                        )))
+	                        .addSecuritySchemes("api_key", new SecurityScheme()
+	                                .type(SecurityScheme.Type.APIKEY)
+	                                .description("Api Key access")
+	                                .in(SecurityScheme.In.HEADER)
+	                                .name("API-KEY")
+	                        )
+	                        .addParameters("Version", new Parameter()
+	                                .in("header")
+	                                .name("Version")
+	                                .schema(new StringSchema())
+	                                .required(false)))*/
+				.components(new Components().addSecuritySchemes("bearer-jwt",
+		                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+		                .in(SecurityScheme.In.HEADER).name("Authorization")))
+				.addSecurityItem(
+	                    new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")))
+	                /*.security(Arrays.asList(
+	                        new SecurityRequirement().addList("spring_oauth"),
+	                        new SecurityRequirement().addList("api_key")))*/
+				.info(new Info()
+				.title("Feira na mão")
+				 .version("2.1a")
+				 .description("Api desenvolvida pelos alunos de graduação e metrado da ufes")
+				 .termsOfService("https://github.com/dwws-ufes/2020-feiranamao")
+				 .license(new License().name("UFES").url("https://github.com/dwws-ufes/2020-feiranamao")));
+	}
+	 private ApiInfo apiInfo() {
+		    return new ApiInfo(
+		      "Feira na mão 1.0",
+		      "Api desenvolvida pelos alunos de graduação e metrado da ufes",
+		      "1.0",
+		      "Terms of service",
+		      new Contact("UFES", "https://github.com/dwws-ufes/2020-feiranamao", "ufes@ufes.com"),
+		      "License of API",
+		      "API license URL",
+		      Collections.emptyList());
+		}
+	 
+	 private ApiKey apiKey() { 
+		    return new ApiKey("Bearer", "Authorization", "header"); 
+		}
+	 
+	 private SecurityContext securityContext() { 
+		    return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+		} 
+
+		private List<SecurityReference> defaultAuth() { 
+		    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+		    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
+		    authorizationScopes[0] = authorizationScope; 
+		    return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+		}
+}
